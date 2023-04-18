bf96bf40632418d8394ea795f64f4031f40c123f
tcrurav <tiburcio.cruz@gmail.com>
2023-04-18 16:30:01 +0100

    FIX search bar

--- src/components/carList/CarList.js
+++ src/components/carList/CarList.js
@@ -1,29 +1,50 @@
+import { useState, useEffect } from "react";
 import CarService from "../../services/car.service";
 import "./CarList.css";
 
 function CarList() {
+  const [cars, setCars] = useState([]);
+  const [query, setQuery] = useState("");
+
   const getCars = () => {
     const allCars = CarService.getCars();
+    setCars(allCars);
+  }
 
+  const showCars = () => {
     return (
-      allCars.map(c => {
-        return (
-          <div className="car-list-item">
-            <div className="car-list-img">
-              <img src={`/assets/img/${c.img}`} alt="car" />
+      cars.filter(c =>
+        c.brand.toLowerCase().includes(query.toLowerCase()) ||
+        c.price.toString().toLowerCase().includes(query.toLowerCase()) ||
+        c.model.toLowerCase().includes(query.toLowerCase()))
+        .map(c => {
+          return (
+            <div className="car-list-item">
+              <div className="car-list-img">
+                <img src={`/assets/img/${c.img}`} alt="car" />
+              </div>
+              <p>{c.brand} - {c.model} - {c.price}</p>
             </div>
-            <p>{c.brand} - {c.model} - {c.price}</p>
-          </div>
-        );
-      })
+          );
+        })
     );
   }
 
+  const handleChange = (event) => {
+    const searchValue = event.target.value;
+    setQuery(searchValue);
+  }
+
+  useEffect(() => {
+    getCars();
+  }, []);
+
   return (
     <>
+      <input type="search" onChange={handleChange} />
       <div className="main-container">
         <div className="car-list-container">
-          {getCars()}
+          {showCars()}
         </div>
         <div className="right-text">
           <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
